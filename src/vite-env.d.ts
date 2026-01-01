ab4dad6e04416f278b84e163f0355fc9a5b2efbb
ExhibitionDefi <your-exhibitiondefi@gmail.com>
2026-01-01 23:26:48 +0100

    update

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -28,7 +28,8 @@ interface ImportMetaEnv {
   readonly VITE_NEXUS_TESTNET_EXPLORER_URL: string
 
   readonly VITE_APP_URL: string
-  readonly VITE_REOWN_PROJECT_ID: string;
+  readonly VITE_REOWN_PROJECT_ID: string
+  readonly VITE_API_URL: string
 
   // Contract Addresses
   readonly VITE_EXHIBITION_ADDRESS: string
